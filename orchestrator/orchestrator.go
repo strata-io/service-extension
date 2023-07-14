0cdba47c1bb7bea493d946dfd9eb45e140bc51d6
Warren Fernandes <warren@strata.io>
2023-07-14 16:07:56 -0600

    Add Logger interface (#3)
    Co-authored-by: Elias Friedman <7999104+eliasjf@users.noreply.github.com>

--- orchestrator/orchestrator.go
+++ orchestrator/orchestrator.go
@@ -1,9 +1,11 @@
 package orchestrator
 
 import (
+	"github.com/strata-io/service-extension/log"
 	"github.com/strata-io/service-extension/secret"
 )
 
 type Orchestrator interface {
 	SecretProvider() (secret.Provider, error)
+	Logger() log.Logger
 }
