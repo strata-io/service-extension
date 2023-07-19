be3a1fa7d8ad53871a06b4bd27d63d13489a9be5
Elias Friedman <7999104+eliasjf@users.noreply.github.com>
2023-07-19 15:49:09 -0700

    Update Logger interface to accept any
    Co-authored-by: Warren Fernandes <warren@strata.io>

--- log/log.go
+++ log/log.go
@@ -9,9 +9,9 @@ package log
 //	log.Info("msg", "example log", "requestID", "1234")
 type Logger interface {
 	// Debug will log at debug level.
-	Debug(keyPairs ...string)
+	Debug(keyPairs ...any)
 	// Info will log at info level.
-	Info(keyPairs ...string)
+	Info(keyPairs ...any)
 	// Error will log at error level.
-	Error(keyPairs ...string)
+	Error(keyPairs ...any)
 }
