654fc7704d38be5d13d76c36ea6466f691efe1a9
Elias Friedman <7999104+eliasjf@users.noreply.github.com>
2023-08-11 14:53:19 -0700

    Add AttributeProvider interface
    Co-authored-by: Daniil <86987714+daniilstrata@users.noreply.github.com>

--- idfabric/idfrabric.go
+++ idfabric/idfrabric.go
@@ -9,3 +9,16 @@ type IdentityProvider interface {
 	// to the underlying IDP to authenticate the user.
 	Login(rw http.ResponseWriter, req *http.Request)
 }
+
+// AttributeProvider is used to retrieve attributes from an external system. A common
+// attribute provider would be a data store such as LDAP.
+type AttributeProvider interface {
+	// Query is used to retrieve attributes for a user. A user's subject and the
+	// requested attributes are consumed as params.
+	//
+	// When a query is successful, key-value pairs of the requested attributes are
+	// returned. When a given AttributeProvider returns a multivalued attribute such
+	// as group memberships, the values are concatenated using a delimiter that is
+	// defined on the Identity Fabric component.
+	Query(sub string, attrs []string) (map[string]string, error)
+}
