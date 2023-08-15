6bd07b51f2a9036aa397a3ec7839cbbcade95819
Elias Friedman <7999104+eliasjf@users.noreply.github.com>
2023-08-15 15:30:36 -0700

    Add docs on how to leverage lib for local extension development, improve Godoc for existing fields (#10)
    Co-authored-by: Daniil <86987714+daniilstrata@users.noreply.github.com>

--- orchestrator/orchestrator.go
+++ orchestrator/orchestrator.go
@@ -9,10 +9,24 @@ import (
 )
 
 type Orchestrator interface {
+	// Logger gets a logger.
 	Logger() log.Logger
+
+	// SessionProvider gets a session provider.
 	SessionProvider() session.Provider
+
+	// SecretProvider gets a secret provider. An error is returned if a secret
+	// provider is not configured.
 	SecretProvider() (secret.Provider, error)
+
+	// IdentityProvider gets an identity provider by name. An error is returned if
+	// the identity provider is not found.
 	IdentityProvider(name string) (idfabric.IdentityProvider, error)
+
+	// AttributeProvider gets an attribute provider by name. An error is returned if
+	// the attribute provider is not found.
 	AttributeProvider(name string) (idfabric.AttributeProvider, error)
+
+	// Router gets a router.
 	Router() router.Router
 }
--- secret/secret.go
+++ secret/secret.go
@@ -1,8 +1,10 @@
 package secret
 
+// Provider is used to retrieve secrets from the configured secret store.
 type Provider interface {
 	// Get retrieves the key from the secret provider.
 	Get(key string) any
+
 	// GetString retrieves the key from the secret provider as a string value.
 	GetString(key string) string
 }
