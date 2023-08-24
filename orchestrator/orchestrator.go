4fd744c6405f9c1b59f7c3b56073d4abd97ae188
Daniil <86987714+daniilstrata@users.noreply.github.com>
2023-08-23 17:04:12 -0700

    Update orchestrator/orchestrator.go
    Co-authored-by: Elias Friedman <7999104+eliasjf@users.noreply.github.com>

--- orchestrator/orchestrator.go
+++ orchestrator/orchestrator.go
@@ -30,6 +30,6 @@ type Orchestrator interface {
 	// Router gets a router.
 	Router() router.Router
 
-	// Metadata gets metadata.
+	// Metadata gets the metadata associated with the Service Extension in use.
 	Metadata() map[string]any
 }
