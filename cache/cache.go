a8679d9703a93272842376297ddb6b54d27dc121
Kevin Nguy <3495280+kewun@users.noreply.github.com>
2024-01-17 16:55:01 -0500

    Expose cache to service extensions.
    Co-authored-by: Daniil <86987714+daniilstrata@users.noreply.github.com>

---
+++ cache/cache.go
@@ -0,0 +1,33 @@
+package cache
+
+import (
+	"context"
+)
+
+type Cache interface {
+	// GetBytes returns the []byte for a given key. If the key does not exist, a
+	// ErrNotFound will be returned.
+	GetBytes(ctx context.Context, key string) ([]byte, error)
+
+	// SetBytes adds a key and the corresponding []byte value the backing store.
+	// If options are passed, they will be configured for the key. Any existing value
+	// for the key will be replaced.
+	SetBytes(ctx context.Context, key string, value []byte, opts ...Option) error
+}
+
+// Options contains Options for a given piece of data.
+type Options struct {
+	// Name of the cache.
+	Name string
+}
+
+// Option is an option to pass to the Cache setters. Option can be used to add
+// additional capabilities or modify the default behavior for how the data is stored.
+type Option func(*Options)
+
+// WithName is an option that allows the ability to specify the cache name.
+func WithName(name string) Option {
+	return func(do *Options) {
+		do.Name = name
+	}
+}
