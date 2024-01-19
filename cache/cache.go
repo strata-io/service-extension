99e027424c5b626608ecc9c9b2fc0bfff5ddf729
Kevin Nguy <3495280+kewun@users.noreply.github.com>
2024-01-19 13:57:33 -0500

    Add TTL option and classify WithName as a constraint.
    Co-authored-by: Daniil <86987714+daniilstrata@users.noreply.github.com>

--- cache/cache.go
+++ cache/cache.go
@@ -1,5 +1,7 @@
 package cache
 
+import "time"
+
 type Cache interface {
 	// GetBytes returns the []byte for a given key. If the key does not exist, a
 	// ErrNotFound will be returned.
@@ -13,17 +15,37 @@ type Cache interface {
 
 // Options contains Options for a given piece of data.
 type Options struct {
-	// Name of the cache.
-	Name string
+	// Represents the Time-To-Live (TTL) for a given piece of data. When this
+	// time elapses, the data will be deleted from the underlying store.
+	TTL time.Duration
 }
 
 // Option is an option to pass to the Cache setters. Option can be used to add
 // additional capabilities or modify the default behavior for how the data is stored.
 type Option func(*Options)
 
-// WithName is an option that allows the ability to specify the cache name.
-func WithName(name string) Option {
+// WithTTL can be used to add a Time-To-Live (TTL) to a given piece of data. When
+// this option is set, it is up to the Store implementations to determine how
+// best to honor this option.
+func WithTTL(ttl time.Duration) Option {
 	return func(do *Options) {
+		do.TTL = ttl
+	}
+}
+
+// Constraints contains Constraints for a given piece of data.
+type Constraints struct {
+	// Name of the cache.
+	Name string
+}
+
+// Constraint is an option to pass to the Cache setters. Option can be used to add
+// additional capabilities or modify the default behavior for how the data is stored.
+type Constraint func(*Constraints)
+
+// WithName is an option that allows the ability to specify the cache name.
+func WithName(name string) Constraint {
+	return func(do *Constraints) {
 		do.Name = name
 	}
 }
