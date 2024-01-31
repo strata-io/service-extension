ea2528bdbcc770f0da64d3a6da2ec732bf3f0d73
Patrick Cutz <patrick@strata.io>
2024-01-31 13:12:29 -0500

    Add godoc for app

--- app/app.go
+++ app/app.go
@@ -1,5 +1,8 @@
 package app
 
+// App enables a way to interact with the application which defines the
+// Service Extension.
 type App interface {
+	// Name returns the name of the application.
 	Name() string
 }
