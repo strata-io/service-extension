85dab42448c83d7575f6ceb5c56102b635b77ef2
Daniil <86987714+daniilstrata@users.noreply.github.com>
2024-11-27 15:43:15 -0800

    Update http/http.go
    Co-authored-by: Elias Friedman <7999104+eliasjf@users.noreply.github.com>

--- http/http.go
+++ http/http.go
@@ -5,7 +5,7 @@ import "net/http"
 // HTTP provides a way to interact with the HTTP client.
 type HTTP interface {
 	// GetClient returns the HTTP client based on the provided name.
-	// If the client does not exist, the second return value will be false.
+	// If the client does not exist, an error will be returned.
 	GetClient(name string) (*http.Client, error)
 
 	// SetClient adds a client to the HTTP client store based on the provided name.
