f40e73620a9f3e01edd7991976a7c514ce5fb5f9
Granville Schmidt <1246157+gramidt@users.noreply.github.com>
2025-11-19 11:29:38 -0700

    move binddn function to type to simplify usage (#46)

--- ldap/ldap.go
+++ ldap/ldap.go
@@ -3,10 +3,7 @@ package ldap
 
 import "context"
 
-// Provider is the interface for LDAP operations.
-type Provider interface {
-	// BindDN retrieves the bind DN from the context. The context should be
-	// retrieved via the Orchestrator's Context() method (e.g., `api.Context()`).
-	// It returns an empty string if no bind DN is present.
-	BindDN(ctx context.Context) string
-}
+// BindDN retrieves the bind DN from the context. The context should be
+// retrieved via the Orchestrator's Context() method (e.g., `api.Context()`).
+// It returns an empty string if no bind DN is present.
+type BindDN func(ctx context.Context) string
